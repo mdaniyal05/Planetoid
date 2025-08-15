b8743b386f5aa0d36c57ace27fa6539bd2f30cfe
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-15 21:45:40 +0500

    Set up and initialize game environment

---
+++ game/src/main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+
+def main():
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("white")
+
+        pygame.display.flip()
+
+        clock.tick(FPS)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
